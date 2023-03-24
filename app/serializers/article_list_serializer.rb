5a2da37f397004db5ffc0f1d08176d6c9d99b6ae
Stellamargy <stella.oloo@student.moringaschool.com>
2023-03-24 13:58:23 +0300

    done

--- app/serializers/article_list_serializer.rb
+++ app/serializers/article_list_serializer.rb
@@ -1,3 +1,4 @@
 class ArticleListSerializer < ActiveModel::Serializer
   attributes :id, :title, :minutes_to_read, :author, :preview, :date
+  # attributes :id, :title, :minutes_to_read, :author, :content
 end
